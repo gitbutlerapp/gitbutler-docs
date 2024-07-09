637c0097b0ab34adec94c0de741f8d1fadd81366
ndom91 <yo@ndo.dev>
2024-07-09 21:39:40 +0200

    feat: add source transformer

--- app/source.ts
+++ app/source.ts
@@ -5,5 +5,53 @@ import { loader } from "fumadocs-core/source"
 export const { getPage, getPages, pageTree, files } = loader({
   baseUrl: "/",
   rootDir: "docs",
+  transformers: [
+    async ({ storage }) => {
+      const releasesFile = storage.files.get("releases.page")
+      if (!releasesFile) {
+        console.log("No releases file found")
+        return
+      }
+
+      // Fetch latest release from GitHub
+      const ghResponse = await fetch(
+        "https://api.github.com/repos/gitbutlerapp/gitbutler/releases/latest"
+      )
+      if (!ghResponse.ok) {
+        console.log(`Failed to fetch latest release: ${ghResponse.statusText}`)
+        return
+      }
+
+      const ghResponsePayload = await ghResponse.json()
+      const latestRelease = Array.isArray(ghResponsePayload)
+        ? ghResponsePayload[0]
+        : ghResponsePayload
+
+      if (latestRelease.draft || latestRelease.prerelease) {
+        console.log("Latest release is a draft or prerelease, skipping update")
+        return
+      }
+
+      const newContent = releasesFile.data.data.exports.structuredData.contents
+
+      newContent.splice(1, 0, {
+        heading: `v${latestRelease.name.replace("release/", "")}`,
+        content: latestRelease.body
+      })
+      // newContent.splice(1, 0, {
+      //   heading: `v111`,
+      //   content: "NEW CONTENT!"
+      // })
+      releasesFile.data.data.exports.structuredData.contents = newContent
+
+      console.log("Updated releases file", JSON.stringify(releasesFile, null, 2))
+      // data.storage.files.set("releases.page", releasesFile)
+      storage.write("releases.mdx", "page", releasesFile)
+
+      // Debug
+      const releasesFile2 = storage.files.get("releases.page")
+      // console.log("releaseFile2", JSON.stringify(releasesFile2, null, 2))
+    }
+  ],
   source: createMDXSource(map)
 })
