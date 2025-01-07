a39754b06121e63906e8cd070475bec25abb459b
ndom91 <yo@ndo.dev>
2025-01-07 14:49:29 +0100

    fix: clean up generate-llmstxt script

--- scripts/generate-llmstxt.js
+++ scripts/generate-llmstxt.js
@@ -2,13 +2,6 @@ import { readdir, readFile, writeFile } from "node:fs/promises"
 
 const CONTENT_DIR = "./content/docs"
 
-function toTitleCase(str) {
-  return str
-    .split("_")
-    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ")
-}
-
 let fileContent = []
 let filePaths = {}
 
@@ -17,28 +10,10 @@ try {
     recursive: true,
     withFileTypes: true
   })
-  // fileContent = await Promise.all(
-  //   files
-  //     .map((file) => {
-  //       if (
-  //         file.isFile() &&
-  //         file.name.includes("mdx") &&
-  //         !file.parentPath.includes("api-reference")
-  //       ) {
-  //         console.log("file:", file)
-  //         return readFile(`${file.parentPath}/${file.name}`)
-  //       }
-  //     })
-  //     .filter(Boolean)
-  // )
 
-  // for await (const file of files) {
   for (const file of files) {
     if (file.isFile() && file.name.includes("mdx") && !file.parentPath.includes("api-reference")) {
-      console.log("file:", file)
-      // console.log("reading file:", file)
       filePaths[file.name] = `${file.parentPath}/${file.name}`
-      // fileContent.push(await readFile(`${file.parentPath}/${file.name}`))
     }
   }
   fileContent = `
@@ -47,27 +22,21 @@ try {
 ## Docs 
 
 ${Object.entries(filePaths).map((file) => {
-  return `- [${file[0]}](${file[1].replace("content/docs", "").replace(".mdx", "").replace("./", "")})\n`
-})}`
+  return `- [${file[0].replace(".mdx","")}](#${file[0]})\n`
+}).join("")}`
 } catch (err) {
   console.error("Error reading files", err)
 }
 
-// const mergedMarkdown = fileContent
-
 await Promise.all(
   Object.entries(filePaths).map(async (file) => {
-    // const fileContents = await readFile(`${file[1]}/${file[0]}`)
     let fileContents = (await readFile(`${file[1]}`)).toString()
     fileContents = fileContents.replace(/---.*---\n/s, "")
     fileContents = fileContents.replace(/import .*\n/g, "")
 
     fileContent += `\n\n# ${file[0]}`
     fileContent += `\n${fileContents}`
-
-    // console.log("content:", fileContents)
   })
 )
-console.log("FILECONTENT:", fileContent)
 
 await writeFile("./public/llms.txt", fileContent)
